19cedd52d1b7a856a81cad36847e147aa04e9f01
franklinthor <31597952+franklinthor@users.noreply.github.com>
2017-09-05 15:09:39 +0000

    Add files via upload

--- authors/index.php
+++ authors/index.php
@@ -8,6 +8,12 @@
 <head>
     <title>Manage Authors</title>
     <link rel="stylesheet" type="text/css" href="../style/authors.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../js/filter.js">
+
+    </script>
 </head>
 <body class="main">
 <ul id="navbarUl">
@@ -18,23 +24,29 @@
     <h1>Manage Authors</h1>
     <p><a href="worker.php?add">Add new author</a></p>
 </div>
+
 <div class= "header2">
     <ul>
         <?php foreach ($authors as $author):?>
 
-                <li id= "list">
-                    <form action="" method="post">
-                        <div class= "submitButton">
+            <li id= "list">
+                <form action="" method="post">
+                    <div class= "submitButton">
+                        <div>
                             <?php echo html($author['name']); ?>
-                            <input type="hidden" name="id" value="<?php echo $author['id']; ?>">
-                            <input type="submit" name="action" value="Edit">
-                            <input type="submit" name="action" value="Delete">
                         </div>
-                    </form>
-                </li>
+                        <input type="hidden" name="id" value="<?php echo $author['id']; ?>">
+                        <input type="submit" name="action" value="Edit">
+                        <input type="submit" name="action" value="Delete">
+                    </div>
+                </form>
+            </li>
 
         <?php endforeach; ?>
     </ul>
+</div>
+<div id="filter" class="form-group pull-right">
+    <input type="text" class="search form-control" placeholder="What you looking for?">
 </div>
 </body>
 </html>
--- categories/index.php
+++ categories/index.php
@@ -7,16 +7,22 @@
 <html>
 <head>
     <title>Manage Categories</title>
-    <link rel="stylesheet" type="text/css" href="../style/authors.css">
+    <link rel="stylesheet" type="text/css" href="../style/categories.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../js/filter.js">
+
+    </script>
 </head>
 <body class="main">
 <ul id="navbarUl">
     <li class="navbarLi"><a class="active" href="../authors/index.php">Authors</a></li>
     <li class="navbarLi"><a href="../categories/index.php">Categories</a></li>
 </ul>
-<div class="header">
-    <h1>Manage Categories</h1>
-    <p><a href="worker.php?add">Add new category</a></p>
+<div>
+    <h1 class="header">Manage Categories</h1>
+    <p id="addNewCategory"><a href="worker.php?add">Add new category</a></p>
 </div>
 <div class= "header2">
     <ul>
@@ -25,7 +31,9 @@
             <li id= "list">
                 <form action="" method="post">
                     <div class= "submitButton">
-                        <?php echo html($category['name']); ?>
+                        <div>
+                             <?php echo html($category['name']); ?>
+                        </div>
                         <input type="hidden" name="id" value="<?php echo $category['id']; ?>">
                         <input type="submit" name="action" value="Edit">
                         <input type="submit" name="action" value="Delete">
@@ -35,6 +43,9 @@
 
         <?php endforeach; ?>
     </ul>
+</div>
+<div id="filter" class="form-group pull-right">
+    <input type="text" class="search form-control" placeholder="What you looking for?">
 </div>
 </body>
 </html>
